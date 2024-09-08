cbcd51680da67373eabef754d5d897bb73a00a5e
Eng-Mohamed <mkayta2023@gmail.com>
2024-09-08 19:08:33 +0300

    I done this Exercise

--- script.js
+++ script.js
@@ -1,1 +1,12 @@
-console.log("hello");
+const grade= 120
+ if(grade<= 100 && grade >= 90){
+    console.log("you got Grade A")
+ }else if(grade<= 90 && grade >= 75){
+    console.log("you got Grade B")
+ }else if(grade<= 74 && grade >= 60){
+    console.log("you got Grade C")
+ }else if(grade<=59 && grade>=0) {
+   console.log("You got an FAILED") 
+ }
+
+
